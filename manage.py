90810af559a0deebc310d7005984cd3bbeb7a7ae
benoitc <benoitc@localhost.metavers.net>
2008-09-05 01:25:47 +0200

    fix email

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright 2008 by Benoît Chesneau <couchit@e-engura.com>
+# Copyright 2008 by Benoît Chesneau <benoitc@e-engura.com>
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
